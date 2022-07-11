783053bd87e2f43147959c57d5b90bab9cd961dd
Aryo <aryoxp@gmail.com>
2022-07-12 06:51:58 +0700

    Add menu ID to Encode-Decode page
    Prettify code in encode-decode.js

--- admin/module/utility/controller/UtilityDecodeController.php
+++ admin/module/utility/controller/UtilityDecodeController.php
@@ -2,7 +2,7 @@
 
 class UtilityDecodeController extends ModuleController {
   function index() {
-    // Core::lib(Core::CONFIG)->set('menu', 'group', CoreConfig::CONFIG_TYPE_CLIENT);
+    Core::lib(Core::CONFIG)->set('menu', 'encode-decode', CoreConfig::CONFIG_TYPE_CLIENT);
     $this->useScript('encode-decode.js');
     $this->render($this->view('encode-decode.php'), ['title' => 'Log Data Encoder-Decoder']);
   }
